f6eea9f353d32546f171569dd13cae03b985880e
nori0313 <ec2-user@ip-172-31-36-133.ec2.internal>
2023-02-23 16:43:58 +0900

    タイトル降順＆ペジネーション（５）

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,6 +19,8 @@
                  </div>
              @endforeach
         </div>
-         
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
     </body>
 </html>
