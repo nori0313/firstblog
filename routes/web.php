213a0d73cd10fd425c403b237e3a66fe12c4c9b1
nori0313 <ec2-user@ip-172-31-36-133.ec2.internal>
2023-03-09 16:17:13 +0900

    ブログ投稿保存処理

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ use App\Http\Controllers\PostController;
 //     return view('posts/index');
 // });
 Route::get('/',[PostController::class,'index']);
+Route::get('/posts/{create}',[PostController::class,'create']);
 Route::get('/posts/{post}',[PostController::class,'show']);
+Route::post('/posts',[PostController::class,'store']);
